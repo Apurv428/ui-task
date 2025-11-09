d8aefed7590e9617edc3aa3b8aad5b03c2c4391d
Apurv428 <apurvsonawane28@gmail.com>
2025-11-09 23:16:54 +0530

    remove unused value

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
 import MobileDashboard from "./pages/MobileDashboard";
